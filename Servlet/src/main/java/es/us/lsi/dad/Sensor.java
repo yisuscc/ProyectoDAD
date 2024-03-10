0712b0d0353c8625c39c724859d64d2a29a2c729
Jesús Carrascosa <72463497+yisuscc@users.noreply.github.com>
2024-03-10 10:52:11 +0100

    Casi terminado el servlet de sensores.
    da error de sesnor esnull cuando intento añadir un sensor nuevo

--- Servlet/src/main/java/es/us/lsi/dad/Sensor.java
+++ Servlet/src/main/java/es/us/lsi/dad/Sensor.java
@@ -7,26 +7,37 @@ private Integer id;
 private String name;
 private String tipo;
 private Long  timestamp;
-@Override
-public String toString() {
-	return "Sensor [id=" + id + ", name=" + name + ", tipo=" + tipo + ", timestamp=" + timestamp + ", value=" + value
-			+ "]";
+private Float value;
+//placaID  es util en el futuro 
+
+
+public static Sensor randomSensor() {
+	Integer id = (int)Math.random();
+	String name = ((Double)Math.random()).toString();
+	String tipo = "CO2";
+	Long timestamp = System.currentTimeMillis();
+	Float value  =(float)Math.random();
+	return new Sensor(id, name, tipo, timestamp, value);
+}public static Sensor randomSensorWithID(Integer id) {
+	
+	String name =((Double)Math.random()).toString();
+	String tipo = "CO2";
+	Long timestamp = System.currentTimeMillis();
+	Float value  =(float)Math.random();
+	return new Sensor(id, name, tipo, timestamp, value);
 }
-@Override
-public int hashCode() {
-	return Objects.hash(id, name, timestamp, tipo, value);
+public Sensor(Integer id, String name, String tipo, long timestamp, float value) {
+	// añadimos los checkers
+	super();
+	this.id = id;
+	this.name = name;
+	this.tipo = tipo;
+	this.timestamp = timestamp;
+	this.value = value;
 }
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
-		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Sensor other = (Sensor) obj;
-	return Objects.equals(id, other.id) && Objects.equals(name, other.name)
-			&& Objects.equals(timestamp, other.timestamp) && Objects.equals(tipo, other.tipo) && value == other.value;
+public Sensor() {
+	super();
+	
 }
 public Integer getId() {
 	return id;
@@ -49,24 +60,36 @@ public void setTipo(String tipo) {
 public Long getTimestamp() {
 	return timestamp;
 }
-public void setTimestamp(Long timestamp) {
+public void setTimestamp(long timestamp) {
 	this.timestamp = timestamp;
 }
-public long getValue() {
+public Float getValue() {
 	return value;
 }
-public void setValue(long value) {
+public void setValue(float value) {
 	this.value = value;
 }
-private long value;
-// private Integer placaId;
-public Sensor(Integer id, String name, String tipo, Long timestamp, long value) {
-	super();
-	this.id = id;
-	this.name = name;
-	this.tipo = tipo;
-	this.timestamp = timestamp;
-	this.value = value;
+@Override
+public int hashCode() {
+	return Objects.hash(id);
 }
+@Override
+public boolean equals(Object obj) {
+	if (this == obj)
+		return true;
+	if (obj == null)
+		return false;
+	if (getClass() != obj.getClass())
+		return false;
+	Sensor other = (Sensor) obj;
+	return Objects.equals(id, other.id);
+}
+@Override
+public String toString() {
+	return "Sensor [id=" + id + ", name=" + name + ", tipo=" + tipo + ", timestamp=" + timestamp + ", value=" + value
+			+ "]";
+}
+
+
 
 }
---
+++ Servlet/src/main/java/es/us/lsi/dad/ServletSensor.java
@@ -0,0 +1,108 @@
+package es.us.lsi.dad;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+
+import com.google.gson.Gson;
+
+/**
+ * Servlet implementation class ServletSensor
+ */
+public class ServletSensor extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private  Sensores sensoresInternos;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+	public void init() throws ServletException{
+		sensoresInternos= Sensores.sensoresRandom(5);
+	}
+//    public ServletSensor() {
+//        super();
+//        // TODO Auto-generated constructor stub
+//    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse resp) throws ServletException, IOException {
+		Integer  sensorId = Integer.parseInt(request.getParameter("sensorId"));
+		if(sensoresInternos.contieneSensor(sensorId)) {
+			String mensaje = sensoresInternos.getSensor(sensorId).toString();
+			resp.setStatus(200);
+			response(resp, mensaje);
+		}else {
+			resp.setStatus(404);
+			response(resp, "El sensor no existe, estos son los sensores existentes \r\n" + sensoresInternos.toString());
+		}
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	/**
+	 *
+	 */
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		BufferedReader reader = req.getReader();
+		Gson gson = new Gson();
+		Sensor sensor = gson.fromJson(reader,Sensor.class);
+		// expandir las condiciones 
+		// esto quzas deberia encargarse la clase sensor con los checkers 
+		Boolean cond = sensor.getId()!= null && sensor.getTimestamp()!= null;
+		if(cond) {
+			sensoresInternos.addSensor(sensor);
+			resp.setStatus(201);
+			resp.getWriter().println(gson.toJson(sensor));
+		}else {
+			resp.setStatus(300);
+			response(resp, "Formato equivocado");
+		}
+		
+		
+	}
+	protected void doUpdate(HttpServletRequest request, HttpServletResponse response) {
+		//TODO
+		//para luego, que si no nos morimos
+	}
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Integer  sensorId = Integer.parseInt(req.getParameter("sensorId"));
+		if(sensoresInternos.contieneSensor(sensorId)) {
+			Boolean res = sensoresInternos.deleteSensor(sensorId);
+			String mensaje = "";
+			if(res) {
+			resp.setStatus(200);
+			
+			mensaje = "El sensor "+ sensorId.toString()+ "se ha eliminado con exito";
+			}
+			response(resp, mensaje);
+		}else {
+			resp.setStatus(404);
+			response(resp, "El sensor no existe");
+		}
+	}
+	public void destroy() {
+		// do nothing.
+	}
+	private void response(HttpServletResponse resp, String msg) throws IOException {
+		//cádigo del profesor
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<t1>" + msg + "</t1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
