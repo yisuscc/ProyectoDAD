358a3623b73cb36c8288b92977d9dbe13d05d778
Jesús Carrascosa <72463497+yisuscc@users.noreply.github.com>
2024-03-22 17:57:37 +0100

    modificado una cosita ligera

--- rest/src/rest/RestServer.java
+++ rest/src/rest/RestServer.java
@@ -28,28 +28,17 @@ public class RestServer extends AbstractVerticle {
 		// Instantiating a Gson serialize object using specific date format
 		gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create(); // ISO-8601 FTW
 		// Definimos el router
-		// que se encarga de coger las apis
-//		Router router = Router.router(vertx);
-//		vertx.createHttpServer().requestHandler(router::handle).listen(8069,result->{
-//			if(result.succeeded()) {
-//				startFuture.complete();
-//			}else {
-//				startFuture.fail("El lanzamiento del servidor ha fallado"+result.cause());
-//				
-//			}
-//		});
-		vertx.createHttpServer().requestHandler(r -> {
-			r.response().end("<h1>Bienvenido a mi primera aplicacion Vert.x 3</h1>"
-					+ "Esto es un ejemplo de una Verticle sencillo para probar el despliegue");
-		}).listen(8089, result -> {
-			if (result.succeeded()) {
-				startFuture.complete();
-			} else {
-				startFuture.fail(result.cause());
-			}
+		// que se encarga de coger las apis y redirigirlas
+	Router router = Router.router(vertx);
+		vertx.createHttpServer().requestHandler(router::handle).listen(8069,result->{			if(result.succeeded()) {
+			startFuture.complete();
+			}else {
+			startFuture.fail("El lanzamiento del servidor ha fallado"+result.cause());
+							}
 		});
+	
 		// Asociamos las funciones a una api
-		// Estructura:
+		// A modo de orientació, esta es la extructura:
 		/*
 		 * router.post("/api/users").handler(this::addOne);
 		 * router.route("/api/users*").handler(BodyHandler.create());
@@ -57,17 +46,17 @@ public class RestServer extends AbstractVerticle {
 		 * router.get("/api/users/user/all").handler(this::getAll);
 		 */
 
-		//router.route("/api/*").handler(BodyHandler.create());
+		router.route("/api/*").handler(BodyHandler.create());
 		// DADOS UNA Placa y un id de sensor o actuador
 		// TODO 1primera api añade una medición
 		// TODO 2 devuelve la ultima medición
-		// TODO 3 devuelve el estado de un actuador;
+		// TODO 3 devuelve el ultimo  estado de un actuador;
 		// TODO 4 añade el estado de un actuador;
 		// Dada una placa id
 		// TODO 5 lo mismo pero devolviendo el ultimo estado de todos los actuadores o
 		// sensores;
-		// OPcionales que no te ocupen mucho tiempo
-		// lo mismo que las 1, 2,3 ,4,5, pero que te de las 5 mas recientes
+		// Opcionales que no te ocupen mucho tiempo
+		// lo mismo que las , 2,3 ,5, pero que te de las x mas recientes
 		//
 
 	}
@@ -75,7 +64,7 @@ public class RestServer extends AbstractVerticle {
 	// creamos el stop
 	public void stop(Promise<Void> stopPromise) throws Exception {
 		try {
-			// TODO LIMPIAR la claseAglutinador.
+			apsa.clear();
 			stopPromise.complete();
 		} catch (Exception e) {
 			stopPromise.fail(e);
