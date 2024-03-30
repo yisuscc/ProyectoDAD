1b5980d9874da12959eaef51945e3621b0e6b83a
Jesús Carrascosa <72463497+yisuscc@users.noreply.github.com>
2024-03-30 11:28:49 +0100

    creada la estructura de las APIS
    el rest server sigue sin lanzar, no se muy bien el porqué

--- rest/src/rest/RestServer.java
+++ rest/src/rest/RestServer.java
@@ -49,18 +49,42 @@ public class RestServer extends AbstractVerticle {
 		router.route("/api/*").handler(BodyHandler.create());
 		// DADOS UNA Placa y un id de sensor o actuador
 		// TODO 1primera api añade una medición
+		router.post("/api/sensor").handler(this::setSensor);
 		// TODO 2 devuelve la ultima medición
+		router.get("/api/sensor/:placaId/:id").handler(this::getSensor);
 		// TODO 3 devuelve el ultimo  estado de un actuador;
+		router.get("/api/actuador/:placaId/:id").handler(this::getActuador);
 		// TODO 4 añade el estado de un actuador;
+		router.post("/api/actuador").handler(this::setActuador);
 		// Dada una placa id
 		// TODO 5 lo mismo pero devolviendo el ultimo estado de todos los actuadores o
-		// sensores;
+		// sensores de una placa dada;
+		router.post("/api/sensores/:placaId").handler(this::getAllSensores);
+		router.post("/api/actuadores/:placaId").handler(this::getAllActuadores);
 		// Opcionales que no te ocupen mucho tiempo
 		// lo mismo que las , 2,3 ,5, pero que te de las x mas recientes
 		//
 
 	}
-
+	// definimos las llamadas del handler 
+	private void setSensor(RoutingContext routingContext) {
+//	TODO 	
+	}
+	private void setActuador(RoutingContext routingContext) {
+		//TODO
+	}
+private void getSensor(RoutingContext routingContext) {
+	//TODO
+}
+private void getActuador(RoutingContext routingContext) {
+	//TODO
+}
+private void getAllSensores(RoutingContext routingContext) {
+	//TODO
+}
+private void getAllActuadores(RoutingContext routingContext) {
+	//TODO
+}
 	// creamos el stop
 	public void stop(Promise<Void> stopPromise) throws Exception {
 		try {
