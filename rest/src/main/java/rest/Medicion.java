25574fe9dbf0d019e0c027c97416128ba4c576b9
Jesús Carrascosa <72463497+yisuscc@users.noreply.github.com>
2024-04-12 12:11:34 +0200

    remodeladas las clases actuadores y medicion

--- rest/src/main/java/rest/Medicion.java
+++ rest/src/main/java/rest/Medicion.java
@@ -3,53 +3,20 @@ package rest;
 import java.util.Objects;
 
 public class Medicion {
-	//almacena los datos 
-	// cada instancia almacena los datos de una medición de un sensor, en un momento dado 
-	//mas que sensor debería ser actuador
-	private Integer id;
+
+	private Integer idSensor;
 	private Integer placaId; //no estoy seguro de que sirva 
 	private Long timestamp;
 	private Double concentracion;
-	public Medicion(Integer id, Integer placaId, Long timestamp, Double concentracion) {
-		super();
-		this.id = id;
-		this.placaId = placaId;
-		this.timestamp = timestamp;
-		this.concentracion = concentracion;
-	}
-	public Integer getId() {
-		return id;
-	}
-	public void setId(Integer id) {
-		this.id = id;
-	}
-	public Integer getPlacaId() {
-		return placaId;
-	}
-	public void setPlacaId(Integer placaId) {
-		this.placaId = placaId;
-	}
-	public Long getTimestamp() {
-		return timestamp;
-	}
-	public void setTimestamp(Long timestamp) {
-		this.timestamp = timestamp;
-	}
-	public Double getConcentracion() {
-		return concentracion;
-	}
-	public void setConcentracion(Double concentracion) {
-		this.concentracion = concentracion;
-	}
-	@Override
-	public String toString() {
-		return "Sensor [id=" + id + ", placaId=" + placaId + ", timestamp=" + timestamp + ", concentracion="
-				+ concentracion + "]";
-	}
+	private Integer idGroup;
+
+
 	@Override
 	public int hashCode() {
-		return Objects.hash(concentracion, id, placaId, timestamp);
+		return Objects.hash(concentracion, idGroup, idSensor, placaId, timestamp);
 	}
+
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -59,13 +26,82 @@ public class Medicion {
 		if (getClass() != obj.getClass())
 			return false;
 		Medicion other = (Medicion) obj;
-		return Objects.equals(concentracion, other.concentracion) && Objects.equals(id, other.id)
-				&& Objects.equals(placaId, other.placaId) && Objects.equals(timestamp, other.timestamp);
+		return Objects.equals(concentracion, other.concentracion) && Objects.equals(idGroup, other.idGroup)
+				&& Objects.equals(idSensor, other.idSensor) && Objects.equals(placaId, other.placaId)
+				&& Objects.equals(timestamp, other.timestamp);
 	}
 
-	public static Medicion random(Integer id , Integer placaId) {
+
+	@Override
+	public String toString() {
+		return "Medicion [idSensor=" + idSensor + ", placaId=" + placaId + ", timestamp=" + timestamp
+				+ ", concentracion=" + concentracion + ", idGroup=" + idGroup + "]";
+	}
+
+
+	public Medicion(Integer idSensor, Integer placaId, Long timestamp, Double concentracion, Integer idGroup) {
+		super();
+		this.idSensor = idSensor;
+		this.placaId = placaId;
+		this.timestamp = timestamp;
+		this.concentracion = concentracion;
+		this.idGroup = idGroup;
+	}
+
+
+	public Integer getIdSensor() {
+		return idSensor;
+	}
+
+
+	public void setIdSensor(Integer idSensor) {
+		this.idSensor = idSensor;
+	}
+
+
+	public Integer getPlacaId() {
+		return placaId;
+	}
+
+
+	public void setPlacaId(Integer placaId) {
+		this.placaId = placaId;
+	}
+
+
+	public Long getTimestamp() {
+		return timestamp;
+	}
+
+
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+
+
+	public Double getConcentracion() {
+		return concentracion;
+	}
+
+
+	public void setConcentracion(Double concentracion) {
+		this.concentracion = concentracion;
+	}
+
+
+	public Integer getIdGroup() {
+		return idGroup;
+	}
+
+
+	public void setIdGroup(Integer idGroup) {
+		this.idGroup = idGroup;
+	}
+
+
+	public static Medicion random(Integer id , Integer placaId,Integer groupId) {
 		Long timestamp = System.currentTimeMillis();
 		Double value = Math.random()*1000;
-		return new Medicion(id, placaId, timestamp, value); 
+		return new Medicion(id, placaId, timestamp, value,groupId); 
 	}
 }
