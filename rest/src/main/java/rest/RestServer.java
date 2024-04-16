9beb92d41ebaaae7e9e0ede746012d097bded0c3
Jesús Carrascosa <72463497+yisuscc@users.noreply.github.com>
2024-04-16 18:42:38 +0200

    intento de getActuador, no se si funciona

--- rest/src/main/java/rest/RestServer.java
+++ rest/src/main/java/rest/RestServer.java
@@ -34,8 +34,8 @@ public class RestServer extends AbstractVerticle {
 
 	public void start(Promise<Void> startFuture) {
 		// creamos datos sinteticos
-		//apsa = AglutinadorPlacaSensorActuador.getRandomData(5);
-		//System.out.println(apsa.toString());
+		// apsa = AglutinadorPlacaSensorActuador.getRandomData(5);
+		// System.out.println(apsa.toString());
 		// COnfiguramos los datos del gson;
 		// Instantiating a Gson serialize object using specific date format
 		gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create(); // ISO-8601 FTW
@@ -46,7 +46,7 @@ public class RestServer extends AbstractVerticle {
 		// mediante la MYSQLConnectOptions.
 		PoolOptions poolOptions = new PoolOptions().setMaxSize(37); // por poner un numero
 		msc = MySQLPool.pool(vertx, connectOptions, poolOptions);
-		
+
 		// Nota:el usuario y contrasena de root es root
 		// Alternativamente PDAD es PDAD.
 
@@ -55,14 +55,14 @@ public class RestServer extends AbstractVerticle {
 		// Definimos el router
 		// que se encarga de coger las apis y redirigirlas
 		Router router = Router.router(vertx);
-		vertx.createHttpServer().requestHandler(router::handle).listen(8060, result -> {
+		vertx.createHttpServer().requestHandler(router::handle).listen(8021, result -> {
 			if (result.succeeded()) {
 				startFuture.complete();
 			} else {
 				startFuture.fail("El lanzamiento del servidor ha fallado" + result.cause());
 			}
 		});
-
+/////////////////////////////////////////////////////////////////////
 		// Asociamos las funciones a una api
 
 		router.route("/api/*").handler(BodyHandler.create());
@@ -82,16 +82,18 @@ public class RestServer extends AbstractVerticle {
 		router.get("/api/actuadores/:placaId").handler(this::getAllActuadores);
 		// TODO: hacer que devuelv todo los valores del os sensores y actuadores de
 		// unmismo group id
-		//TODO: Hacer una devuelva el historico de todos los valores de un sensor o actuador;
+		// TODO: Hacer una devuelva el historico de todos los valores de un sensor o
+		// actuador;
 		// Opcionales que no te ocupen mucho tiempo
 		// lo mismo que las , 2,3 ,5, pero que te de las x mas recientes
 		// TODO: hacer una que de la última medición a partir de una hora dada?
-	
+
 	}
 
+////////////////////////////////////////////////////////////////////////////
 	// definimos las llamadas del handler
 	private void setSensor(RoutingContext routingContext) {
-		//TODO: ADAPTAR
+		// TODO: ADAPTAR
 		final Medicion medicion = gson.fromJson(routingContext.getBodyAsString(), Medicion.class);
 		apsa.addSensor(medicion);
 		routingContext.response().setStatusCode(201).putHeader("content-type", "application/json; charset=utf-8")
@@ -99,7 +101,7 @@ public class RestServer extends AbstractVerticle {
 	}
 
 	private void setActuador(RoutingContext routingContext) {
-		//TODO: ADAPTAR
+		// TODO: ADAPTAR
 		final Actuador actuador = gson.fromJson(routingContext.getBodyAsString(), Actuador.class);
 		apsa.addActuador(actuador);
 		routingContext.response().setStatusCode(201).putHeader("content-type", "application/json; charset=utf-8")
@@ -107,63 +109,94 @@ public class RestServer extends AbstractVerticle {
 	}
 
 	private void getSensor(RoutingContext routingContext) {
-		//TODO: ADAPTAR
+		// TODO: No funciona
 		final Integer placaId = Integer.parseInt(routingContext.request().getParam("placaId"));
 		final Integer id = Integer.parseInt(routingContext.request().getParam("id"));
-		// Boolean cond = placaId!=null && id != null && apsa.existeSensor(id, placaId);
-//		Medicion medicion = apsa.getLastSensor(id, placaId);
-//		System.out.println(medicion);
-//		if (medicion != null) {
-//			routingContext.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(200)
-//					.end(gson.toJson(medicion));
-//		} else {
-//			// devuelve un errocete
-//
-//			routingContext.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(404)
-//					.end();
-//		}
-		msc.getConnection(cn-> {
-			if(cn.succeeded()) {
-				cn.result().preparedQuery("SELECT * FROM mediciones WHERE medicionId = ? AND placaId= ? ORDER BY fecha DESC LIMIT 1 ").
-				execute(Tuple.of(id,placaId),res->{
-					if(res.succeeded()) {
-					RowSet<Row> resultSet = res.result();
-					List<Medicion> result = new ArrayList<>();	
-					for(Row elem : resultSet) {
-						result.add(new Medicion(elem.getInteger("medicionId"), elem.getInteger("placaId"),elem.getLong("fecha"),elem.getDouble("concentracion"),elem.getInteger("groupId")));
-					}
-					System.out.println(result.to);
-					}else {
-						
-					}
-				});
-			}else {
+		System.out.println("se ejecuta la petición");
+		msc.getConnection(cn -> {
+			if (cn.succeeded()) {
+				cn.result().preparedQuery(
+						"SELECT * FROM mediciones WHERE medicionId = ? AND placaId= ?")
+						.execute(Tuple.of(id, placaId), res -> {
+							if (res.succeeded()) {
+								System.out.println("Funciona lA QUERY");
+								RowSet<Row> resultSet = res.result();
+								List<Medicion> result = new ArrayList<>();
+								for (Row elem : resultSet) {
+									result.add(new Medicion(elem.getInteger("medicionId"), elem.getInteger("placaId"),
+											elem.getLong("fecha"), elem.getDouble("concentracion"),
+											elem.getInteger("groupId")));
+								}
+								// imprime el mensaje por consola pero no lo envia
+								System.out.println(result.toString());
+								// lo enviamos en el response header con un 200
+								routingContext.response().putHeader("content-type", "application/json; charset=utf-8")
+										.setStatusCode(200).end(gson.toJson(result));
+							} else {
+								System.out.println("Error: " + res.cause().getLocalizedMessage());
+
+							}
+							cn.result().close();
+						});
+			} else {
 				System.out.println(cn.cause().toString());
-				
+				routingContext.response().putHeader("content-type", "application/json; charset=utf-8")
+						.setStatusCode(404).end();
+
 			}
 		});
 	}
 
 	private void getActuador(RoutingContext routingContext) {
-		//TODO: ADAPTAR
-//	final Integer placaId = routingContext.queryParams().contains("placaId")?Integer.parseInt(routingContext.queryParam("placaId").get(0)):null;
-//	final Integer id = routingContext.queryParams().contains("id")?Integer.parseInt(routingContext.queryParam("id").get(0)):null;
+		// TODO: ADAPTAR
 		final Integer placaId = Integer.parseInt(routingContext.request().getParam("placaId"));
 		final Integer id = Integer.parseInt(routingContext.request().getParam("id"));
-	
-		Actuador actuador = apsa.getLastActuador(id, placaId);
-		if (actuador != null) {
-			routingContext.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(200)
-					.end(gson.toJson(actuador));
-		} else {
-			routingContext.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(404)
-					.end();
-		}
-
+		String query = "SELECT * FROM actuadores WHERE placaId = ? AND actuadorId = ? ORDER BY fecha DESC LIMIT 1";
+		msc.getConnection(con-> {
+			if(con.succeeded()) {
+				// si la conexion ha tenido exíto 
+				//hacemos la query 
+				con.result().preparedQuery(query).
+				execute(Tuple.of(placaId,id),res -> {
+					
+			if(res.succeeded()) {
+				// si la query ha tenido exito
+				// cogemos el resul set
+				RowSet<Row> resultSet = res.result();
+				List<Actuador> result= new ArrayList<>();
+				for(Row elem : resultSet) {
+					// Actuador(Integer idActuador, Integer placaId, Long timestamp, Boolean status, Integer idGroup)
+					result.add(new Actuador(elem.getInteger("actuadorId"),elem.getInteger("placaId"),elem.getLong("fecha"),elem.getBoolean("statusValue"),elem.getInteger("idGroup"))); //TODO: Meter los elementos
+				
+				}
+				//para que aparezca en el terminal 
+				System.out.println(result.toString());
+				routingContext.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(200).
+				end(gson.toJson(result));
+			}else {
+				// si la query no ha tenido exito 
+				routingContext.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(404).
+				end();
+			}
+			//cerramos la conexion 
+			con.result().close();
+				});
+				
+			}else {
+				// si la conexion no ha tenido exito
+				//imprimimos un mensaje de error
+				System.out.println("Error:"+con.cause().toString());
+				// adicionalmente devolmvemos un mensaje de error en elos headers 
+				// un 500 o un 400? 
+				//Creo que un 500 ya que es problema entre vertx y la bbdd
+				routingContext.response().putHeader("content-type", "application/json; charset=utf-8").setStatusCode(500)
+				.end();
+			}
+		});
 	}
 
 	private void getAllSensores(RoutingContext routingContext) {
-		//TODO: ADAPTAR
+		// TODO: ADAPTAR
 		final Integer placaId = Integer.parseInt(routingContext.request().getParam("placaId"));
 		// final Integer placaId =
 		// routingContext.queryParams().contains("placaId")?Integer.parseInt(routingContext.queryParam("placaId").get(0)):null;
@@ -174,7 +207,7 @@ public class RestServer extends AbstractVerticle {
 	}
 
 	private void getAllActuadores(RoutingContext routingContext) {
-		//TODO: ADAPTAR
+		// TODO: ADAPTAR
 		final Integer placaId = Integer.parseInt(routingContext.request().getParam("placaId"));
 		List<Actuador> lsAux = placaId != null && apsa.existePlaca(placaId) ? apsa.getLastActuadoresList(placaId)
 				: new ArrayList<Actuador>();
@@ -196,68 +229,66 @@ public class RestServer extends AbstractVerticle {
 
 	////////////////////////////////////////////////////////////////////////////////////
 	// Funciones Auxiliares
-	
+
 	private void insertMedicion(Medicion med) {
-				// Creo que es mejor no usarla 
-	
-			msc.getConnection(c-> {
-				c.result().preparedQuery("INSERT INTO Proyecto_DAD.mediciones(medicionId, placaId, concentracion, fecha, groupId) VALUES (?,?,?,?,?)").
-				execute(Tuple.of(med.getIdSensor(),med.getPlacaId(),med.getConcentracion(),med.getTimestamp(),med.getIdGroup()), r->{
-				if(r.succeeded()) {
-					
-				}else {
-					System.out.println("Error:"+r.cause().getLocalizedMessage());
-				}
-				});
-			});
-		
+		// Creo que es mejor no usarla
+
+		msc.getConnection(c -> {
+			c.result().preparedQuery(
+					"INSERT INTO Proyecto_DAD.mediciones(medicionId, placaId, concentracion, fecha, groupId) VALUES (?,?,?,?,?)")
+					.execute(Tuple.of(med.getIdSensor(), med.getPlacaId(), med.getConcentracion(), med.getTimestamp(),
+							med.getIdGroup()), r -> {
+								if (r.succeeded()) {
+
+								} else {
+									System.out.println("Error:" + r.cause().getLocalizedMessage());
+								}
+							});
+		});
 
 	}
 
 	private void insertActuador(Actuador act) {
-		msc.getConnection(c-> {
-			c.result().preparedQuery("INSERT INTO Proyecto_DAD.actuadores(actuadorId, placaId, statusValue, fecha, groupId) VALUES (?,?,?,?,?)").
-			execute(Tuple.of(act.getIdActuador(),act.getPlacaId(),act.getStatus(),act.getTimestamp(),act.getIdGroup()), r->{
-			if(r.succeeded()) {
-				
-			}else {
-				System.out.println("Error:"+r.cause().getLocalizedMessage());
-			}
-			});
+		msc.getConnection(c -> {
+			c.result().preparedQuery(
+					"INSERT INTO Proyecto_DAD.actuadores(actuadorId, placaId, statusValue, fecha, groupId) VALUES (?,?,?,?,?)")
+					.execute(Tuple.of(act.getIdActuador(), act.getPlacaId(), act.getStatus(), act.getTimestamp(),
+							act.getIdGroup()), r -> {
+								if (r.succeeded()) {
+
+								} else {
+									System.out.println("Error:" + r.cause().getLocalizedMessage());
+								}
+							});
 		});
 	}
 
 	private void insertPlaca(Placa placa) {
-		
-	
-		msc.getConnection(c-> {
-			c.result().preparedQuery("INSERT INTO placas(placaId) VALUES (?)").
-			execute(Tuple.of(placa.getId()), r->{
-			if(r.succeeded()) {
-				
-			}else {
-				System.out.println("Error:"+r.cause().getLocalizedMessage());
-			}
+
+		msc.getConnection(c -> {
+			c.result().preparedQuery("INSERT INTO placas(placaId) VALUES (?)").execute(Tuple.of(placa.getId()), r -> {
+				if (r.succeeded()) {
+
+				} else {
+					System.out.println("Error:" + r.cause().getLocalizedMessage());
+				}
 			});
 		});
 	}
 
 	private void generaRandomData() {
 		try {
-			final int nGroup= 5;// numero de grupos
+			final int nGroup = 5;// numero de grupos
 			final int nElementos = 5;// numero de sensores y actuadores por cada grupo
 			int nPlaca = 0;
-			for(int g = 0; g<nGroup;g++) {
-				for(int e=0; e<nElementos;e++) {
+			for (int g = 0; g < nGroup; g++) {
+				for (int e = 0; e < nElementos; e++) {
 					insertPlaca(new Placa(nPlaca));
 					Medicion med = Medicion.random(e, nPlaca, g);
 					insertMedicion(med);
-					System.out.println(med);
-					nPlaca++;
-					insertPlaca(new Placa(nPlaca));
 					insertActuador(Actuador.random(e, nPlaca, g));
 					nPlaca++;
-					
+
 				}
 			}
 		} catch (Exception e) {
