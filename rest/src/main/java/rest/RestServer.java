c0a526875fb2a5ca9467ab8b4ca0536a678532f1
Jesús Carrascosa <72463497+yisuscc@users.noreply.github.com>
2024-05-03 19:47:29 +0200

    supuesta terminada la parte del bac kend
    Falta por testearla, pero espero que funcione.

--- rest/src/main/java/rest/RestServer.java
+++ rest/src/main/java/rest/RestServer.java
@@ -17,11 +17,15 @@ import java.util.stream.IntStream;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import io.netty.handler.codec.mqtt.MqttQoS;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.buffer.Buffer;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 import io.vertx.ext.web.handler.BodyHandler;
+import io.vertx.mqtt.MqttClient;
+import io.vertx.mqtt.MqttClientOptions;
 import io.vertx.mysqlclient.MySQLClient;
 import io.vertx.mysqlclient.MySQLConnectOptions;
 import io.vertx.mysqlclient.MySQLPool;
@@ -31,32 +35,39 @@ import io.vertx.sqlclient.RowSet;
 import io.vertx.sqlclient.Tuple;
 
 public class RestServer extends AbstractVerticle {
+	// parmetros modificables:
+	final String ipMqttServer = "localhost";
+	final Integer puertoAPIRest = 8039;
+	final Double umbral = 0.0;
+	// variabless privadas e inmodificables
 	private Gson gson;
-	private AglutinadorPlacaSensorActuador apsa;
 	private MySQLPool msc;
+	private MqttClient mqttClient;
 
 	public void start(Promise<Void> startFuture) {
 
-		// COnfiguramos los datos del gson;
+		// 1)Configuramos los datos del gson;
 		// Instantiating a Gson serialize object using specific date format
 		gson = new GsonBuilder().setDateFormat("yyyy-MM-dd").create(); // ISO-8601 FTW
 
-		// Conexíon con la base de datos.
+		// 2) Nos conectamos a la base de datos.
 		MySQLConnectOptions connectOptions = new MySQLConnectOptions().setPort(3306).setHost("localhost")
 				.setDatabase("proyecto_dad").setUser("root").setPassword("root");
 		// mediante la MYSQLConnectOptions.
 		PoolOptions poolOptions = new PoolOptions().setMaxSize(37); // por poner un numero
 		msc = MySQLPool.pool(vertx, connectOptions, poolOptions);
 
-		// Nota:el usuario y contrasena de root es root
-		// Alternativamente PDAD es PDAD.
+		/*
+		 * Nota:el usuario y contrasena de root es root Alternativamente PDAD es PDAD.
+		 */
+		// 3) inicializamos el mqtt
+		mqttClient = MqttClient.create(vertx, new MqttClientOptions().setAutoKeepAlive(true));
+		mqttClient.connect(1883, ipMqttServer, s -> System.out.println("Conexión al broker"));
 
-		// creamos datos sintéticos
-		// Los datos sinteticos están en la bd
-		// Definimos el router
+		// 4)Definimos el router
 		// que se encarga de coger las apis y redirigirlas
 		Router router = Router.router(vertx);
-		vertx.createHttpServer().requestHandler(router::handle).listen(8039, result -> {
+		vertx.createHttpServer().requestHandler(router::handle).listen(puertoAPIRest, result -> {
 			if (result.succeeded()) {
 				startFuture.complete();
 			} else {
@@ -82,11 +93,12 @@ public class RestServer extends AbstractVerticle {
 		router.get("/api/sensores/:placaId").handler(this::getAllSensores);
 		router.get("/api/actuadores/:placaId").handler(this::getAllActuadores);
 
-		//  Hacer que devuelva los ultimos valores de todos los sensores y actuadores de
+		// Hacer que devuelva los ultimos valores de todos los sensores y actuadores de
 		// un mismo group id
 		router.get("/api/lastactuadorGroupId/:groupId").handler(this::getLastActuadorGroupId);
 		router.get("/api/lastsensorGroupId/:groupId").handler(this::getLastSensorGroupId);
-		//Devuelve todos los valores de todos los valores y sensores de un mismo group id
+		// Devuelve todos los valores de todos los valores y sensores de un mismo group
+		// id
 		router.get("/api/allactuadorGroupId/:groupId").handler(this::getAllActuadorGroupID);
 		router.get("/api/allsensorGroupId/:groupId").handler(this::getAllSensorGroupID);
 		// Hacer una devuelva el historico de todos los valores de un sensor o
@@ -103,9 +115,11 @@ public class RestServer extends AbstractVerticle {
 	// Idoneamente, para los gets creariamos una funcion auxiliar que se encarga del
 	// al conexion a la base de datos.
 	private void setSensor(RoutingContext routingContext) {
-		// Furula fino señoras y señores.
+// TODO: añadir la conectividad mqtt para activar los actuadores de un group id 
+		// cuando se sobrepasa un umbral
 		final Medicion medicion = gson.fromJson(routingContext.getBodyAsString(), Medicion.class);
 		String query = "INSERT INTO Proyecto_DAD.mediciones(medicionId, placaId, concentracion, fecha, groupId) VALUES (?,?,?,?,?)";
+		String topic = medicion.getIdGroup().toString();
 		msc.getConnection(con -> {
 			if (con.succeeded()) {
 				// si la conexion ha tenido exíto
@@ -119,6 +133,32 @@ public class RestServer extends AbstractVerticle {
 								System.out.println("Insertado: " + medicion.toString());
 								routingContext.response().putHeader("content-type", "application/json; charset=utf-8")
 										.setStatusCode(200).end(gson.toJson(medicion));
+								// TODO Lógica mqttt;
+								// 1) obtenemos la ultima medición de cada sensor:
+								List<Medicion> ultimasMediciones = obtainLastMeasurementsFromGroupId(
+										medicion.getIdGroup());// TODO implementar
+								/*
+								 * Esto dependerá de la logica que implementemos: 2 opciones: 1. que cuando
+								 * cualquiera de los sensores supere el umbral se active 2.(descartada) que
+								 * cuando la media de los sensores supere el umbral se activen
+								 */
+								// 2) evaluamos la condicion
+								Boolean condicion = false;// alguno de los sensores es mayor o igual que el umbral
+								for (Medicion m : ultimasMediciones) {
+									if (m.getConcentracion() >= umbral)
+										;
+									condicion = true;
+									break;
+								}
+								if (condicion) {
+									// encendemos todos los sensores
+
+									sendMessage(topic, "1");
+								} else {
+									// apagamos todos los sensores
+									sendMessage(topic, "0");
+								}
+
 							} else {
 								// si la query no ha tenido exito
 								routingContext.response().putHeader("content-type", "application/json; charset=utf-8")
@@ -142,7 +182,7 @@ public class RestServer extends AbstractVerticle {
 	}
 
 	private void setActuador(RoutingContext routingContext) {
-		// TODO: ADAPTAR
+		// TODO añadir la logíca de mqtt
 		final Actuador actuador = gson.fromJson(routingContext.getBodyAsString(), Actuador.class);
 		String query = "INSERT INTO Proyecto_DAD.actuadores(actuadorId, placaId, statusValue, fecha, groupId) VALUES (?,?,?,?,?)";
 		msc.getConnection(con -> {
@@ -282,40 +322,40 @@ public class RestServer extends AbstractVerticle {
 	}
 
 	private void getAllSensores(RoutingContext routingContext) {
-		 
-	final Integer placaId = Integer.parseInt(routingContext.request().getParam("placaId"));
-	String query = "SELECT * FROM mediciones WHERE placaId = ?";
-	Tuple tupla = Tuple.of(placaId);
-	retrieveSensorDB(query,tupla, routingContext);
+
+		final Integer placaId = Integer.parseInt(routingContext.request().getParam("placaId"));
+		String query = "SELECT * FROM mediciones WHERE placaId = ?";
+		Tuple tupla = Tuple.of(placaId);
+		retrieveSensorDB(query, tupla, routingContext);
 	}
 
 	private void getAllActuadores(RoutingContext routingContext) {
-		
+
 		final Integer placaId = Integer.parseInt(routingContext.request().getParam("placaId"));
 		String query = "SELECT * FROM actuadores WHERE placaId = ?";
 		Tuple tupla = Tuple.of(placaId);
 		retrieveActuadorDB(query, tupla, routingContext);
-		
+
 	}
 
 	private void getLastActuadorGroupId(RoutingContext routingContext) {
 		// devuelve los ultimos valores de tods los actyuadores de unmismo group id
-		
+
 		final Integer groupId = Integer.parseInt(routingContext.request().getParam("groupId"));
 		String query = "SELECT * FROM actuadores WHERE groupId = ?";
 		Tuple tupla = Tuple.of(groupId);
 		msc.getConnection(con -> {
 			if (con.succeeded()) {
-				
+
 				System.out.println("Conexion exitosa");
 				con.result().preparedQuery(query).execute(tupla, res -> {
 
 					if (res.succeeded()) {
-						
+
 						RowSet<Row> resultSet = res.result();
 						List<Actuador> result = new ArrayList<>();
 						for (Row elem : resultSet) {
-							
+
 							result.add(new Actuador(elem.getInteger("actuadorId"), elem.getInteger("placaId"),
 									elem.getLong("fecha"), elem.getBoolean("statusValue"), elem.getInteger("groupId")));
 
@@ -344,27 +384,27 @@ public class RestServer extends AbstractVerticle {
 						.setStatusCode(500).end();
 			}
 		});
-		
+
 	}
 
 	private void getLastSensorGroupId(RoutingContext routingContext) {
 		// devuelve los ultimos valores de todos los sensores de unmismo group id
-		
+
 		final Integer groupId = Integer.parseInt(routingContext.request().getParam("groupId"));
 		String query = "SELECT * FROM mediciones WHERE groupId = ?";
 		Tuple tupla = Tuple.of(groupId);
 		msc.getConnection(con -> {
 			if (con.succeeded()) {
-				
+
 				System.out.println("Conexion exitosa");
 				con.result().preparedQuery(query).execute(tupla, res -> {
 
 					if (res.succeeded()) {
-						
+
 						RowSet<Row> resultSet = res.result();
 						List<Medicion> result = new ArrayList<>();
 						for (Row elem : resultSet) {
-							
+
 							result.add(new Medicion(elem.getInteger("medicionId"), elem.getInteger("placaId"),
 									elem.getLong("fecha"), elem.getDouble("concentracion"),
 									elem.getInteger("groupId")));
@@ -396,18 +436,21 @@ public class RestServer extends AbstractVerticle {
 		});
 
 	}
-private void getAllSensorGroupID(RoutingContext routingContext) {
-	final Integer groupId = Integer.parseInt(routingContext.request().getParam("groupId"));
-	String query = "SELECT * FROM mediciones WHERE groupId = ?";
-	Tuple tuple = Tuple.of(groupId);
-	retrieveSensorDB(query, tuple, routingContext);
-}
-private void getAllActuadorGroupID(RoutingContext routingContext) {
-	final Integer groupId = Integer.parseInt(routingContext.request().getParam("groupId"));
-	String query = "SELECT * FROM actuadores WHERE groupId = ?";
-	Tuple tuple = Tuple.of(groupId);
-	retrieveActuadorDB(query, tuple, routingContext);
-}
+
+	private void getAllSensorGroupID(RoutingContext routingContext) {
+		final Integer groupId = Integer.parseInt(routingContext.request().getParam("groupId"));
+		String query = "SELECT * FROM mediciones WHERE groupId = ?";
+		Tuple tuple = Tuple.of(groupId);
+		retrieveSensorDB(query, tuple, routingContext);
+	}
+
+	private void getAllActuadorGroupID(RoutingContext routingContext) {
+		final Integer groupId = Integer.parseInt(routingContext.request().getParam("groupId"));
+		String query = "SELECT * FROM actuadores WHERE groupId = ?";
+		Tuple tuple = Tuple.of(groupId);
+		retrieveActuadorDB(query, tuple, routingContext);
+	}
+
 	private void getSensor(RoutingContext routingContext) {
 		// Devuelve la última medición de un sensor
 		final Integer placaId = Integer.parseInt(routingContext.request().getParam("placaId"));
@@ -525,22 +568,82 @@ private void getAllActuadorGroupID(RoutingContext routingContext) {
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////
-	// Funciones Auxiliares olvidarse de ellas, son perjudiciales para la salud
-	//////////////////////////////////////////////////////////////////////////////////// 
-	private static List<Medicion>ultimoSensoresGroupID(List<Medicion> sensores){
-		Collection<Medicion> coleccion= sensores.stream().collect(Collectors.
-				groupingBy(m-> Par.of(m.getIdSensor(),m.getPlacaId()),
-						HashMap::new,Collectors.collectingAndThen(Collectors.maxBy(
-								Comparator.comparing(Medicion::getTimestamp)),Optional::get))).values();
+	// Funciones Auxiliares
+	////////////////////////////////////////////////////////////////////////////////////
+	private void sendMessage(String topic, String mensaje) {
+
+		mqttClient.connect(1883, "localhost", s -> {
+			mqttClient.publish(topic, Buffer.buffer(mensaje), MqttQoS.AT_LEAST_ONCE, false, false);
+			});
+
+	}
+
+	private List<Medicion> obtainLastMeasurementsFromGroupId(Integer idGroup) {
+		// TODO: COmprobar funciionamiento
+		List<Medicion> resultados = new ArrayList<Medicion>();
+		String query = "SELECT * FROM mediciones WHERE groupId = ?";
+		Tuple tupla = Tuple.of(idGroup);
+
+		msc.getConnection(con -> {
+			if (con.succeeded()) {
+
+				System.out.println("Conexion exitosa");
+				con.result().preparedQuery(query).execute(tupla, res -> {
+
+					if (res.succeeded()) {
+
+						RowSet<Row> resultSet = res.result();
+						List<Medicion> result = new ArrayList<>();
+						for (Row elem : resultSet) {
+
+							result.add(new Medicion(elem.getInteger("medicionId"), elem.getInteger("placaId"),
+									elem.getLong("fecha"), elem.getDouble("concentracion"),
+									elem.getInteger("groupId")));
+
+						}
+						result = ultimoSensoresGroupID(result);
+						resultados.addAll(result);
+
+					} else {
+						// si la query no ha tenido exito
+						System.out.println("Error en la query");
+
+					}
+					// cerramos la conexion
+					con.result().close();
+
+				});
+
+			} else {
+				// si la conexion no ha tenido exito
+				// imprimimos un mensaje de error
+
+			}
+		});
+
+		return resultados;
+	}
+
+	private static List<Medicion> ultimoSensoresGroupID(List<Medicion> sensores) {
+		Collection<Medicion> coleccion = sensores.stream()
+				.collect(
+						Collectors.groupingBy(m -> Par.of(m.getIdSensor(), m.getPlacaId()), HashMap::new,
+								Collectors.collectingAndThen(
+										Collectors.maxBy(Comparator.comparing(Medicion::getTimestamp)), Optional::get)))
+				.values();
 		return new ArrayList<Medicion>(coleccion);
 	}
-	private static List<Actuador>ultimoActuadoresGroupID(List<Actuador> actuadores){
-		Collection<Actuador> coleccion= actuadores.stream().collect(Collectors.
-				groupingBy(a->Par.of(a.getIdActuador(),a.getPlacaId()),
-						HashMap::new,Collectors.collectingAndThen(Collectors.maxBy(
-								Comparator.comparing(Actuador::getTimestamp)),Optional::get))).values();
+
+	private static List<Actuador> ultimoActuadoresGroupID(List<Actuador> actuadores) {
+		Collection<Actuador> coleccion = actuadores.stream()
+				.collect(
+						Collectors.groupingBy(a -> Par.of(a.getIdActuador(), a.getPlacaId()), HashMap::new,
+								Collectors.collectingAndThen(
+										Collectors.maxBy(Comparator.comparing(Actuador::getTimestamp)), Optional::get)))
+				.values();
 		return new ArrayList<Actuador>(coleccion);
 	}
+
 	private void retrieveSensorDB(String query, Tuple tuple, RoutingContext routingContext) {
 		msc.getConnection(con -> {
 			if (con.succeeded()) {
@@ -634,6 +737,7 @@ private void getAllActuadorGroupID(RoutingContext routingContext) {
 		});
 	}
 
+///////////////Funciones Obsoletas//////////////////////////////////////////////
 	private void insertMedicion(Medicion med) {
 		// Creo que es mejor no usarla
 
